566bc91d2db59dd3295ad5c3a71634eeccad7080
Sacha Chua <sacha@sachachua.com>
2024-12-21 18:56:17 -0500

    add some Lisp code

---
+++ emacstv.el
@@ -0,0 +1,61 @@
+;;; emacstv.el --- Index Emacs-related videos   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Sacha Chua
+
+;; Author: Sacha Chua <sacha@sachachua.com>
+;; Keywords: multimedia
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defvar emacstv-index-org (expand-file-name "videos.org" (file-name-directory (or load-file-name (buffer-file-name))))
+	"*Where the data is stored.")
+
+(defun emacstv-add-from-youtube (url)
+	(interactive "MYouTube URL: ")
+	(let* ((json-object-type 'alist)
+				 (json-array-type 'list)
+				 data)
+		(with-current-buffer (url-retrieve-synchronously url)
+			(set-buffer-multibyte t)
+			(when (re-search-forward "ytInitialPlayerResponse *= *" nil t)
+				(setq data (json-read))))
+		(let-alist (alist-get 'videoDetails data)
+			(if (org-find-property "YOUTUBE_URL" url)
+					(goto-char (org-find-property "YOUTUBE_URL" url))
+				(goto-char (point-max))
+				(unless (bolp) (insert "\n"))
+				(insert "* " .title "\n")
+				(org-entry-put (point) "YOUTUBE_URL" url)
+				(insert (org-ascii--indent-string .shortDescription 2) "\n\n"))
+			(org-entry-put (point) "DATE" (let-alist data .microformat.playerMicroformatRenderer.publishDate))
+			(org-entry-put (point) "SPEAKERS" .author)
+			(org-entry-put (point) "DURATION" (format-seconds "%02h:%z%02m:%02s" (string-to-number .lengthSeconds))))))
+
+(defun emacstv-add-from-org ()
+	(interactive)
+	(when (derived-mode-p 'org-mode)
+		(let ((link (org-element-property :raw-link (org-element-context))))
+			(when (string-match "youtu\\.?be" link)
+				(with-current-buffer (find-file-noselect emacstv-index-org)
+					(emacstv-add-from-youtube link)
+					(display-buffer-in-side-window (current-buffer) nil))))))
+
+(provide 'emacstv)
+;;; emacstv.el ends here
