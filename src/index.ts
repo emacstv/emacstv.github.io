8d5b795ca4f5d9648dfab3267acc0f8e5c4d17ae
Sacha Chua <sacha@sachachua.com>
2025-01-21 20:58:00 -0500

    try to improve accessibility

--- src/index.ts
+++ src/index.ts
@@ -136,6 +136,11 @@ class RandomPickRenderer {
       handler: () => this.store.refresh()
     });
     handlers.push({
+      nodeId: 'random-pick-heading',
+      listenerName: 'keypress',
+      handler: () => this.store.refresh()
+    });
+    handlers.push({
       nodeId: 'die',
       listenerName: 'click',
       handler: () => this.store.refresh()
@@ -187,7 +192,7 @@ Your browser does not support the video tag.
 			handlers: handlers,
 			html: `
 <div>
-  <h2 id="random-pick-heading"><span>Lucky pick</span><span id="die">🎲</span></h2>
+  <h2 id="random-pick-heading" role="button" aria-label="Random" tabindex="0"><span>Lucky pick</span><span id="die">🎲</span></h2>
   ${player}
   <div class="video--caption item">
   ${video.html}
