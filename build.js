a491eb23471cf58a4e98015dd008536705ee857c
xenodium <8107219+xenodium@users.noreply.github.com>
2025-02-01 14:05:02 +0000

    Normalize URL comparison (maybe for real this time)

--- build.js
+++ build.js
@@ -286,7 +286,7 @@ const htmlContent = `
           url.searchParams.delete('speakers');
         }
 
-        if (url.toString() !== new URL(window.location.href).toString()) {
+        if (url.search !== new URL(window.location.href).search) {
           console.log('Replacing location: ' + window.location.href + ' ====> ' + url.toString());
           history.replaceState(null, '', url.toString());
         }
